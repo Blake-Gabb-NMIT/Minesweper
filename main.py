8920460032f318123852d18247c834b9e0064472
blake <blakegabb@gmail.com>
2022-02-23 14:00:05 +1300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import gen
+
+
+
+###
+
+grid = 5 #this is the sise of the screen
+bombs = 3 #how many bombs are in the sqaure
+
+#filling out maps with info
+bombLocationsX, bombLocationsY = gen.bombMaker(bombs,grid) # this will be where the game gens and stores the bombs location to then be added into the viusal display
+Visualmap = gen.FreshMap(grid)#this is the map that the user will see, starts off as all blanks
+Computersmap = False #this will be the full map and what it should look like how the map looks like fully complete
+
+
+
+
+
+
+### 
